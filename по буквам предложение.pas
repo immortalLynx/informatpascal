215533c6ca979b7f7f29984498bef35e98ce411a
Alex Sergeev <69537233+immortalLynx@users.noreply.github.com>
2020-12-27 22:32:19 +0300

    Apply suggestions from code review

--- по буквам предложение.pas
+++ по буквам предложение.pas
@@ -2,14 +2,13 @@
 var sentense:string;
 var i, index: integer;
 begin
-length :=0;
 index := 1;
 writeln('Введите предложение');
 readln(sentense);
 writeln(sentense);
-length := length(pred1);
 
-while index <= length do begin
+while index <= length(sentense) do begin
+
 writeln(sentense[index]);
 index := index + 1;
 end;
