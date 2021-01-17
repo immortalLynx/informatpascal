8efaecd929e30c8a551ee3b4a6b735f106738374
Alex Sergeev <69537233+immortalLynx@users.noreply.github.com>
2021-01-17 19:07:53 +0300

    Add files via upload

---
+++ сумма первых 2х чисел.pas
@@ -0,0 +1,11 @@
+﻿program sum;
+var a,b,c,d: integer;
+begin
+writeln('Введите двухзначное число, а я постичаю их сумму');
+readln(c);
+a := c div 10; 
+b := c mod 10;
+d := a+b;
+writeln('Сумма первых двух чисел равна ',d);
+writeln('a = ',a, ' b = ',b);
+end.
---
+++ четные числа которые делятся на 3 без остатка.pas
@@ -0,0 +1,19 @@
+﻿program hundred;
+var i, a, sum: integer;
+begin
+a := 0;
+i := 0;
+sum := 0;
+while i <= 100 do begin
+    if (a mod 2 = 0)  and (a mod 3 = 0) then begin
+    writeln( i, ' из ста - четное  и делится на 3 без остатка');
+    sum := sum + 1;
+    end;
+    
+     i := i+1;
+     a := a + 1;
+     
+     end;
+
+writeln('Таких чисел - ', sum)
+end.
