fed716c47abb4967ba2f27c649378da95c39d52d
Sergey Sergeev <gurugray@yandex.ru>
2021-01-19 11:09:10 +0300

    нужно расставлять отступы для читаемости
    отступы нужны не для выполнения программы, а для того чтобы код можно было прочитать и понять его структуру.
    код стоит разделять на логические блоки, например ввод / вывод / рассчёт
    
    числа и цифры — это разные сущности, в этой программе ты считаешь сумму цифр, одного числа.

--- сумма первых 2х чисел.pas
+++ сумма первых 2х чисел.pas
@@ -1,11 +1,15 @@
-﻿program sum;
+program sum;
+
 var a,b,c,d: integer;
+
 begin
-writeln('Введите двухзначное число, а я постичаю их сумму');
-readln(c);
-a := c div 10; 
-b := c mod 10;
-d := a+b;
-writeln('Сумма первых двух чисел равна ',d);
-writeln('a = ',a, ' b = ',b);
+  writeln('Введите двухзначное число, а я постичаю сумму первых двух цифр');
+  readln(c);
+
+  a := c div 10; 
+  b := c mod 10;
+  d := a+b;
+
+  writeln('Сумма первых двух цифр равна ', d);
+  writeln('a = ', a, ' b = ', b);
 end.
